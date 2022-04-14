9ce244c47cd1330cd3cee27c39e6d550024384bf
Praveen Kumar <praveenscience@gmail.com>
2022-04-14 11:12:00 +0100

    Completed the About Page dynamically.

--- gen.php
+++ gen.php
@@ -98,33 +98,31 @@
         <div class="container">
           <div class="section-title">
             <h2>About</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+            <p><?php echo $content["About"]["Desc"]; ?></p>
           </div>
 
           <div class="row">
             <div class="col-lg-4" data-aos="fade-right"><img src="<?php echo $content["PersonalInfo"]["Avatar"]; ?>" class="img-fluid" alt="" /></div>
             <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-              <h3>UI/UX Designer &amp; Web Developer.</h3>
-              <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <h3><?php echo $content["About"]["Designation"]; ?></h3>
+              <p class="fst-italic"><?php echo $content["About"]["DesigDesc"]; ?></p>
               <div class="row">
                 <div class="col-lg-6">
                   <ul>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+<?php foreach ($content["About"]["Details"][0] as $key => $value) { ?>
+                      <li><i class="bi bi-chevron-right"></i> <strong><?php echo $key; ?>:</strong> <span><?php echo $value; ?></span></li>
+<?php } ?>
                   </ul>
                 </div>
                 <div class="col-lg-6">
                   <ul>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-                    <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+<?php foreach ($content["About"]["Details"][1] as $key => $value) { ?>
+                      <li><i class="bi bi-chevron-right"></i> <strong><?php echo $key; ?>:</strong> <span><?php echo $value; ?></span></li>
+<?php } ?>
                   </ul>
                 </div>
               </div>
-              <p>Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.</p>
+              <p><?php echo $content["About"]["AboutDesc"]; ?></p>
             </div>
           </div>
         </div>
@@ -136,41 +134,19 @@
         <div class="container">
           <div class="section-title">
             <h2>Facts</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+            <p><?php echo $content["About"]["Facts"]["Desc"]; ?></p>
           </div>
 
           <div class="row no-gutters">
+<?php foreach ($content["About"]["Facts"]["Counters"] as $counter) { ?>
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
               <div class="count-box">
-                <i class="bi bi-emoji-smile"></i>
-                <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-                <p><strong>Happy Clients</strong> consequuntur quae</p>
+                <i class="bi bi-<?php echo $counter["Icon"]; ?>"></i>
+                <span data-purecounter-start="0" data-purecounter-end="<?php echo $counter["Value"]; ?>" data-purecounter-duration="1" class="purecounter"></span>
+                <p><strong><?php echo $counter["Title"]; ?></strong><br /><?php echo $counter["Desc"]; ?></p>
               </div>
             </div>
-
-            <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
-              <div class="count-box">
-                <i class="bi bi-journal-richtext"></i>
-                <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
-                <p><strong>Projects</strong> adipisci atque cum quia aut</p>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
-              <div class="count-box">
-                <i class="bi bi-headset"></i>
-                <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
-                <p><strong>Hours Of Support</strong> aut commodi quaerat</p>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
-              <div class="count-box">
-                <i class="bi bi-people"></i>
-                <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
-                <p><strong>Hard Workers</strong> rerum asperiores dolor</p>
-              </div>
-            </div>
+<?php } ?>
           </div>
         </div>
       </section>
